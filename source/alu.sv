1f71a655acc8b9f388e76911958a1c004bdeba84
[6~Suppatach Sabpisal <ssabpisa@purdue.edu>
2014-09-01 16:38:50 -0400

    ALU

--- source/alu.sv
+++ source/alu.sv
@@ -13,41 +13,57 @@ module alu(
         output logic zero
     );
 
-parameter LSL=4'b1000, LSR=4'b1001, AND=4'b0000, OR=4'b0001,
-          XOR=4'b1111,NOR=4'b1100, ADD=4'b0010, SUB=4'b0110, SLS=4'b0111;
+//TODO: need to verify overflow, negative flag is correct
+assign zero = (output_port == 32'h0 ? 1'b1 : 1'b0);
+assign overflow = !(Port_A[31] != Port_B[31] ? 0 : (Port_B == output_port ? 0 :
+1));
+assign negative = output_port[31];
 
 always_comb begin : decoder
-    case(ALUOP)
+    casez (ALUOP)
         default: begin
-            overflow = 1'b0;
             output_port <= 32'b0;
         end
-        LSL: begin
+        ALU_SLL: begin
             //Logical Shift Left Port A by Port B
             //TODO: if Port B is larger than 32 or other cases
-            overflow = 1'b0;
             output_port = Port_A << Port_B;
         end
-        LSR: begin
+        ALU_SRL: begin
             //Logical Shift Right similar to LSL
-            overflow = 1'b0;
             output_port = Port_A >> Port_B;
         end
-        AND: begin
-            overflow = 1'b0;
+        ALU_AND: begin
             output_port = Port_B & Port_A;
         end
-        OR: begin
-            overflow = 1'b0;
+        ALU_OR: begin
             output_port = Port_B | Port_A;
         end
-        XOR: begin
-            overflow = 1'b0;
+        ALU_XOR: begin
             output_port = (~Port_B) & Port_A;
         end
-        NOR: begin
+        ALU_SLT: begin
+            //Set if less than
+            if (Port_A < Port_B) begin
+               output_port = 1'b1;
+            else begin
+               output_port = 1'b0;
+            end
+
+        end
+        ALU_ADD: begin
+            //Signed Add
+            output_port = Port_A + Port_B;
+        end
+        ALU_SUB: begin
+           //Signed Subtract
+            output_port = Port_A - Port_B;
+        end
+        ALU_NOR: begin
             overflow = 1'b0;
             output_port = ~(Port_B | Port_A);
         end
     endcase
 end
+
+endmodule
