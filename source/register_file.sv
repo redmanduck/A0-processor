a73c8ae7735e2b46ecfe045dba00dec0a767e9e4
[6~Suppatach Sabpisal <ssabpisa@purdue.edu>
2014-09-01 18:52:40 -0400

    Register File compiles

--- source/register_file.sv
+++ source/register_file.sv
@@ -5,20 +5,22 @@
   register file
 
 */
+`include "register_file_if.vh";
 import cpu_types_pkg::*;
 
 module register_file (
   input logic clk,
   input logic n_rst,
-  input register_file_if.rf rfif
+  register_file_if.rf rfif
 );
 
 word_t regs; //no need to specify [31:0] right?
 
-always_ff (posedge clk, negedge n_rst) begin
+always_ff @(posedge clk, negedge n_rst) begin
   if (n_rst == 1'b0) begin
     //Reset all modifiable locations to a value of 0x00000000
     regs = 8'b00000000;
+  end
   else if (rfif.WEN) begin
     //Write mode
     regs[rfif.wsel] <= rfif.wdat;
@@ -26,5 +28,7 @@ always_ff (posedge clk, negedge n_rst) begin
 end
 
 //connects rdat to requested register (rsel) if write enable is low
-assign rfif.rdat1 = (!rfif.WEN ? regs[rfif.rsel1] : 1'b0)
-assign rfif.rdat2 = (!rfif.WEN ? regs[rfif.rsel2] : 1'b0)
+assign rfif.rdat1 = (!rfif.WEN ? regs[rfif.rsel1] : 1'b0);
+assign rfif.rdat2 = (!rfif.WEN ? regs[rfif.rsel2] : 1'b0);
+
+endmodule
