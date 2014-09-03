7801fb7a0165679de495944acc7c0d8e40010951
[6~Suppatach Sabpisal <ssabpisa@purdue.edu>
2014-09-03 10:37:23 -0400

    Mult sim fine

--- asmFiles/mult.asm
+++ asmFiles/mult.asm
@@ -1,16 +1,11 @@
-# *** Multiply program ***
-# takes two operands from stack
-#
-# Suppatach Pat Sabpisal
-# ssabpisa@purdue.edu
-
-  org    0x0000
-  addiu  $sp, $0, 0xFFFC     # Initialize stack pointer to address 0xFFFC
-  addi   $sp, $sp, -8        # allocate 2 spaces
-  lui    $15, 2              # operands
-  lui    $16, 3
-  sw     $15, 4($29)         # put on stack
-  sw     $16, 8($29)
+ org    0x0000
+ addiu  $sp, $0, 0xFFFC     # Initialize stack pointer to address 0xFFFC
+ addi  $sp, $sp, -8        # allocate 2 spaces
+ lui    $15, 2              # operands
+ lui    $16, 3
+ push   $15
+ push   $16
+ halt
 mult:
   pop    $16
   pop    $17
@@ -20,5 +15,7 @@ mloop:
   beq    $17,  $0, mult_done   # exit condition
   addu   $18,  $16, $16        # add s0 to itself
   addi   $17,  $17, -1         # decrement loop
+  j      mloop
 mult_done:
   push   $18
+  addi   $31, $18, 0
