bf2c8a8c2c6fa81cb5831e29ae2e26bdada2cb03
[6~Suppatach Sabpisal <ssabpisa@purdue.edu>
2014-09-08 10:45:02 -0400

    Memory Control close

--- source/memory_control.sv
+++ source/memory_control.sv
@@ -22,4 +22,65 @@ module memory_control (
   // number of cpus for cc
   parameter CPUS = 2;
 
+
+ /*
+    ccif <- dREN = cache data read enable
+    ccif <-  dWEN = cache data write enable
+    ccif <- iREN = cache instruction read enable
+    ccif <- dstore = cache data store ??
+    ccif <- iaddr = cache instruction address
+    ccif <- daddr = data instruction address
+
+    ccif <- ramload = ram load
+    ccif <- ramstate = ram state
+    ...
+*/
+always_comb begin //cache control operations
+    casez(ccif.ramstate)
+      FREE: begin
+        //the memory is free for both I and D
+        ccif.iwait = 1'b0;
+        ccif.dwait = 1'b0o;
+      end
+      ACCESS: begin
+        // memory is begin accessed
+        // data is prioritized
+        // if data is begin read or written
+        // Instruction Fetch will wait
+
+        ccif.iwait = ccif.dREN | ccif.dWEN;
+      end
+      BUSY: begin
+        //memory is being read
+        ccif.iwait = 1'b1;
+        ccif.dwait = 1'b1;
+      end
+      ERROR: begin
+        //block all operations
+        ccif.iwait = 1'b1;
+        ccif.dwait = 1'b1;
+      end
+      default: begin
+        ccif.iwait = 1'b0;
+        ccif.dwait = 1'b0;
+      end
+   endcase
+end
+
+always_comb begin //RAM control operations
+    ccif.ramREN = 1'b0;
+    ccif.ramaddr = 32'b0;
+    if(ccif.dREN) begin
+      //if cache wants to read from RAM
+      ccif.ramREN = 1'b1;
+      ccif.ramaddr = ccif.daddr;
+    end else if (ccif.dWEN) begin
+      ccif.ramWEN = 1'b1;
+      ccif.ramaddr = ccif.daddr;
+    end else if (ccif.iREN) begin
+      ccif.ramREN = 1'b1;
+      ccif.ramaddr = ccif.daddr;
+    end
+end
+
 endmodule
