6d80257d6740100de5b3626907e41104a667a1aa
[6~Suppatach Sabpisal <ssabpisa@purdue.edu>
2014-09-10 16:18:13 -0400

    Writing ram dump tb

--- source/datapath.sv
+++ source/datapath.sv
@@ -22,4 +22,36 @@ module datapath (
   // pc init
   parameter PC_INIT = 0;
 
+  //@Lab3, implements only R-type, load-store instructions
+  logic [5:0] opcode, funct;
+  logic [4:0] rs, rt, rd, shamt;
+  word_t PC_next;
+  word_t PC;
+
+  //map the instruction
+  assign opcode = dpif.imemload[31:26];
+  assign rs = dpif.imemload[26:21];
+  assign rt = dpif.imemload[21:16];
+  assign rd = dpif.imemload[16:11];
+  assign shamt = dpif.imemload[11:6];
+  assign funct = dpif.imemload[6:0];
+  assign immediate = dpif.imemload[16:0];
+
+  always_comb begin : PC_ns_logic
+    if (!dpif.halt) begin
+      PC_next = PC + 4;
+    end
+  end
+
+  always_ff @ (posedge CLK, negedge nRST) begin : PC_update_logic
+    if(!n_RST) begin
+      PC <= PC_INIT;
+    end else begin
+      PC <= PC_next;
+    end
+  end
+
+  always_comb begin :
+
+  always_ff @ (posedge CLK, negedge nRST) begin : PC_Load_stuff
 endmodule
--- source/memory_control.sv
+++ source/memory_control.sv
@@ -2,6 +2,9 @@
   Eric Villasenor
   evillase@gmail.com
 
+  Pat Suppatach Sabpisal
+  ssabpisa@purdue.edu
+
   this block is the coherence protocol
   and artibtration for ram
 */
@@ -21,11 +24,11 @@ module memory_control (
 
   // number of cpus for cc
   parameter CPUS = 2;
-
+  parameter CPUID = 0;
 
  /*
     ccif <- dREN = cache data read enable
-    ccif <-  dWEN = cache data write enable
+    ccif <- dWEN = cache data write enable
     ccif <- iREN = cache instruction read enable
     ccif <- dstore = cache data store ??
     ccif <- iaddr = cache instruction address
@@ -35,12 +38,20 @@ module memory_control (
     ccif <- ramstate = ram state
     ...
 */
-always_comb begin //cache control operations
-    casez(ccif.ramstate)
+
+//Load cache into ram
+assign ccif.iload[CPUID] = ccif.ramload;
+assign ccif.dload[CPUID] = ccif.ramload;
+assign ccif.ramstore[CPUID] = ccif.dstore;
+
+always_comb begin : control_operations
+    ccif.iwait = 2'b0;
+    ccif.dwait = 2'b0;
+    casez (ccif.ramstate)
       FREE: begin
         //the memory is free for both I and D
-        ccif.iwait = 1'b0;
-        ccif.dwait = 1'b0o;
+        ccif.iwait[CPUID] = 1'b0;
+        ccif.dwait[CPUID] = 1'b0;
       end
       ACCESS: begin
         // memory is begin accessed
@@ -48,36 +59,39 @@ always_comb begin //cache control operations
         // if data is begin read or written
         // Instruction Fetch will wait
 
-        ccif.iwait = ccif.dREN | ccif.dWEN;
+        ccif.iwait[CPUID] = ccif.dREN | ccif.dWEN;
       end
       BUSY: begin
         //memory is being read
-        ccif.iwait = 1'b1;
-        ccif.dwait = 1'b1;
+        ccif.iwait[CPUID] = 1'b1;
+        ccif.dwait[CPUID] = 1'b1;
       end
       ERROR: begin
         //block all operations
-        ccif.iwait = 1'b1;
-        ccif.dwait = 1'b1;
+        ccif.iwait[CPUID] = 1'b1;
+        ccif.dwait[CPUID] = 1'b1;
       end
       default: begin
-        ccif.iwait = 1'b0;
-        ccif.dwait = 1'b0;
+        ccif.iwait[CPUID] = 1'b0;
+        ccif.dwait[CPUID] = 1'b0;
       end
    endcase
 end
 
-always_comb begin //RAM control operations
+always_comb begin : RAM_control_mux
     ccif.ramREN = 1'b0;
     ccif.ramaddr = 32'b0;
+    //Prioritizes the data
     if(ccif.dREN) begin
-      //if cache wants to read from RAM
+      //if cache wants to read from RAM (data)
       ccif.ramREN = 1'b1;
       ccif.ramaddr = ccif.daddr;
     end else if (ccif.dWEN) begin
+     //if cache wants to write to RAM (data)
       ccif.ramWEN = 1'b1;
       ccif.ramaddr = ccif.daddr;
     end else if (ccif.iREN) begin
+     //if cache wants to read Instruction from RAM
       ccif.ramREN = 1'b1;
       ccif.ramaddr = ccif.daddr;
     end
