125bf6674e2cd00c14bad0a564b46ad4fd392378
[6~Suppatach Sabpisal <ssabpisa@purdue.edu>
2014-09-22 22:51:20 -0400

    Pipeline register sort of done

--- source/pipeline_registers.sv
+++ source/pipeline_registers.sv
@@ -6,17 +6,19 @@ import cpu_types_pkg::*;
 */
 
 module pipeline_if_id(
-   input logic CLK, nRST, WEN
+   input logic CLK, nRST, WEN,
+   if_id_reg_if.ifid ifid
 );
    word_t next_address;
    word_t instruction;
 
    always_ff @(posedge CLK, negedge nRST) begin
+     if (!nRST) begin
       instruction <= 1'b0;
       next_address <= 1'b0;
    end else if (WEN = 1'b1) begin
-      instruction <= plif.instruction_in;
-      next_address <= plif.next_adderss_in;
+      instruction <= ifid.instruction_in;
+      next_address <= ifid.next_adderss_in;
    end
 endmodule
 
@@ -25,7 +27,8 @@ endmodule
 */
 
 module pipeline_ex_mem(
-   input logic CLK, nRST, WEN
+   input logic CLK, nRST, WEN,
+   ex_mem_reg_if.xmem xmem
 );
 
    //WB control regs
@@ -41,20 +44,28 @@ module pipeline_ex_mem(
 
    always_ff @(posedge CLK, negedge nRST) begin
      if (!nRST) begin
-      WB_MemToReg <= '0;
-      WB_RegWrite <= '0;
-      M_Branch <= '0;
-      M_MemRead <= '0;
-      M_MemWrite <= '0;
-      alu_zero <= '0;
-   end else if (WEN = 1'b1) begin
-      WB_MemToReg <= plif.WB_MemToReg_in;
-      WB_RegWrite <= plif.WB_RegWrite_in;
-      M_Branch <= plif.M_Branch_in;
-      M_MemRead <= plif.M_MemRead_in;
-      M_MemWrite <= plif.M_MemWrite_in;
-      alu_zero <= plif.alu_zero_in;
-   end
+        WB_MemToReg <= '0;
+        WB_RegWrite <= '0;
+        M_Branch <= '0;
+        M_MemRead <= '0;
+        M_MemWrite <= '0;
+        alu_zero <= '0;
+        alu_output <= '0;
+        adder_result <= '0;
+        regfile_rdat2 <= '0;
+        reg_instr <= '0;
+     end else if (WEN = 1'b1) begin
+        WB_MemToReg <= xmem.WB_MemToReg_in;
+        WB_RegWrite <= xmem.WB_RegWrite_in;
+        M_Branch <= xmem.M_Branch_in;
+        M_MemRead <= xmem.M_MemRead_in;
+        M_MemWrite <= xmem.M_MemWrite_in;
+        alu_zero <= xmem.alu_zero_in;
+        alu_output <= xmem.alu_output;
+        adder_result <= xmem.adder_result;
+        regfile_rdat2 <= xmem.regfile_rdat2;
+        reg_instr <= xmem.reg_instr;
+     end
    end
 
 endmodule
@@ -66,7 +77,7 @@ endmodule
 
 module pipeline_id_ex(
    input logic CLK, nRST, WEN
-
+   id_ex_reg_if.idex idex
 );
    //WB control regs
    reg WB_MemToReg, WB_RegWrite;
@@ -84,19 +95,60 @@ module pipeline_id_ex(
    word_t rt;
    word_t rd;
 
+   //ASK ERIC WHY DO WE NEED ANOTHER MEMORY CONTROL
+
    always_ff @(posedge CLK, negedge nRST) begin
-
-   end else if(WEN = 1'b1) begin
-
+      if(nRST) begin
+         WB_MemToReg <= idex.WB_MemToReg_in;
+         WB_RegWrite <= idex.WB_RegWrite_in;
+         M_Branch <= idex.M_Branch_in;
+         M_MemRead <= idex.M_MemRead_in;
+         M_MemWrite <= idex.M_MemWrite_in;
+         EX_RegDst <= idex.EX_RegDst_in;
+         EX_ALUOp <= idex.EX_ALUOp_in;
+         EX_ALUSrc <= idex.EX_ALUSrc_in;
+         next_address <= idex.next_address_in;
+         rdat1 <= idex.rdat1_in;
+         rdat2 <= idex.rdat2_in;
+         sign_ext32 <= idex.sign_ext32_in;
+         rt <= idex.rt_in;
+         rd <= idex.rd_in;
+      end else if(WEN = 1'b1) begin
+         WB_MemToReg <= '0;
+         WB_RegWrite <= '0;
+         M_Branch <= '0;
+         M_MemRead <= '0;
+         M_MemWrite <= '0;
+         EX_RegDst <= '0;
+         EX_ALUOp <= '0;
+         EX_ALUSrc <= '0;
+         next_address <= '0;
+         rdat1 <= '0;
+         rdat2 <= '0;
+         sign_ext32 <= '0;
+         rt <= '0;
+         rd <= '0;
+      end
    end
 
 endmodule
 
 module pipeline_mem_wb(
-   input logic CLK, nRST, WEN
+  input logic CLK, nRST, WEN
+  mem_wb_reg_if.mwb mwb
 );
 
-   word_t _dmemload;
-   word_t _dmemaddr;
+   word_t dmemload;
+   word_t dmemaddr;
+   always_ff @(posedge CLK, negedge nRST) begin
+      if(nRST) begin
+         dmemload <= '0;
+         dmemaddr <= '0;
+      end else if(WEN = 1'b1) begin
+         dmemload <= mwb.dmemload_in;
+         dmemaddr <= mwb.dmemaddr_in;
+      end
+   end
+
 
 endmodule
