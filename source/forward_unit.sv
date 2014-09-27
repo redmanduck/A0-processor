b5d05ee0fd49cf684eeb4a92d9836604a1872d21
Hao Xiong <hxiong@purdue.edu>
2014-09-27 16:49:44 -0400

    forward unit rs = rt case tried solving

--- source/forward_unit.sv
+++ source/forward_unit.sv
@@ -11,6 +11,6 @@ module forward_unit(forward_unit_if.fwu fw_if
 assign fw_if.forwardA = (fw_if.mem_rd == fw_if.ex_rs) ? 1 : ((fw_if.wb_rd == fw_if.ex_rs) ? 2 : 0);
 
 // forwardB choose right value to forward for operend B
-assign fw_if.forwardB = (fw_if.mem_rd == fw_if.ex_rt) ? 1 : ((fw_if.wb_rd == fw_if.ex_rt) ? 2 : 0);
+assign fw_if.forwardB =(fw_if.ex_rs== fw_if.ex_rt) ? 0 : (fw_if.mem_rd == fw_if.ex_rt) ? 1 : ((fw_if.wb_rd == fw_if.ex_rt) ? 2 : 0);
 
 endmodule
