3235972f719e3ca8d31eecd1d6771f1a4d719ae1
Hao Xiong <hxiong@purdue.edu>
2014-09-27 17:26:16 -0400

    forward unit takes care of branch and jump

--- source/forward_unit.sv
+++ source/forward_unit.sv
@@ -8,9 +8,9 @@ module forward_unit(forward_unit_if.fwu fw_if
 );
 
 // forwardA choose right value to forward for operend A
-assign fw_if.forwardA = (fw_if.mem_rd == fw_if.ex_rs) ? 1 : ((fw_if.wb_rd == fw_if.ex_rs) ? 2 : 0);
+assign fw_if.forwardA = (fw_if.regWr && fw_if.regRd) && ((fw_if.mem_rd == fw_if.ex_rs) ? 1 : ((fw_if.wb_rd == fw_if.ex_rs) ? 2 : 0));
 
 // forwardB choose right value to forward for operend B
-assign fw_if.forwardB =(fw_if.ex_rs== fw_if.ex_rt) ? 0 : (fw_if.mem_rd == fw_if.ex_rt) ? 1 : ((fw_if.wb_rd == fw_if.ex_rt) ? 2 : 0);
+assign fw_if.forwardB =(fw_if.regWr && fw_if.regRd) && ((fw_if.ex_rs== fw_if.ex_rt) ? 0 : (fw_if.mem_rd == fw_if.ex_rt) ? 1 : ((fw_if.wb_rd == fw_if.ex_rt) ? 2 : 0));
 
 endmodule
