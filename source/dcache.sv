8c6daa8240bedd42ac7fdd0f5f08fe292efe0d76
[6~Suppatach Sabpisal <ssabpisa@purdue.edu>
2014-10-23 17:11:20 -0400

    Eric-style probably work but doenst synt

--- source/dcache.sv
+++ source/dcache.sv
@@ -37,6 +37,12 @@ module dcache (
   logic hit_out, hit0, hit1;
   logic tag_match0, tag_match1;
   logic cur_lru, rq_blockoffset;
+
+
+  logic which_word, write_dirty, write_valid, CACHE_WEN;
+  word_t write_data;
+  logic [25:0] write_tag;
+
   //current LRU based on index
   assign cur_lru = LRU[rq_index];
   //requested index and tag
@@ -102,83 +108,155 @@ module dcache (
      end
   end
 
+  //STUFF
+  always_ff @ (posedge CLK, negedge nRST) begin
+    if(!nRST) begin
+      LRU <= '0;
+    end else if(hit_out) begin
+      LRU[rq_index] <= !LRU[rq_index];
+    end
+  end 
+
+
+ assign dpif.dmemload = (hit0 ? cway[0].dtable[rq_index].block[rq_blockoffset] : (hit1 ? cway[1].dtable[rq_index].block[rq_blockoffset] : 32'hbadbeef1 )); //or bad1bad1
+
   always_comb begin : output_logic_fsm
-
+    CACHE_WEN = 0;
+    
     casez(state)
-      reset: begin
-        //reset both ways
-        cway[0].dtable = '0;
-        cway[1].dtable = '0;
-        LRU = '0;
-      end
       flush: begin
-
           $display("FLUSHING (! bits)");
       end
       idle: begin
-            if(hit_out && dpif.dmemREN)  begin 
-               LRU[rq_index] = hit0;
-               dpif.dmemload = (hit0 ? cway[0].dtable[rq_index].block[rq_blockoffset] : (hit1 ? cway[1].dtable[rq_index].block[rq_blockoffset] : 32'hbadbeef1 )); //or bad1bad1
+            ccif.dREN = 0;
+            if(hit_out && dpif.dmemWEN) begin
+                $display("idle WRITING SW lru = %d, idx = %d", cur_lru, rq_index);
+
+                // cway[cur_lru].dtable[rq_index].block[rq_blockoffset] = dpif.dmemstore;
+                // cway[cur_lru].dtable[rq_index].dirty = 1;
+                // cway[cur_lru].dtable[rq_index].valid = 1;
+                CACHE_WEN = 1;
+                which_word = rq_blockoffset;
+                write_dirty = 1;
+                write_valid = 1;
+                write_tag = rq_tag;
+                write_data = dpif.dmemstore;
+
             end
+
+            // if(hit_out) begin
+            //     LRU[rq_index] = !LRU[rq_index];
+            // end
+
       end
       fetch1: begin
           ccif.dREN = 1;
-          $display("Fetching 1 %h",  ccif.daddr);
-          cway[cur_lru].dtable[rq_index].block[0] = ccif.dload[CPUID];
-          cway[cur_lru].dtable[rq_index].dirty = 0;
-          cway[cur_lru].dtable[rq_index].tag = rq_tag;
-          cway[cur_lru].dtable[rq_index].valid = 0;
+        //  $display("Fetching 1 %h",  ccif.daddr);
+          // cway[cur_lru].dtable[rq_index].block[0] = ccif.dload[CPUID];
+          // cway[cur_lru].dtable[rq_index].dirty = 0;
+          // cway[cur_lru].dtable[rq_index].tag = rq_tag;
+          // cway[cur_lru].dtable[rq_index].valid = 0;
+
+          CACHE_WEN = 1;
+          which_word = 0;
+          write_dirty = 0;
+          write_tag = rq_tag;
+          write_valid = 0;
+          write_data = ccif.dload[CPUID];
+
           ccif.daddr = {rq_tag, rq_index, 3'b000};
       end
       fetch2: begin
           ccif.dREN = 1;
           //fetch two block offset = 1
+          // ccif.daddr = {rq_tag, rq_index, 3'b100};
+          // cway[cur_lru].dtable[rq_index].tag = rq_tag;
+          // cway[cur_lru].dtable[rq_index].block[1] = ccif.dload[CPUID];
+          // cway[cur_lru].dtable[rq_index].dirty = 0;
+          CACHE_WEN = 1;
+          which_word = 1;
+          write_dirty = 0;
+          write_tag = rq_tag;
+          write_data = ccif.dload[CPUID];
+
           ccif.daddr = {rq_tag, rq_index, 3'b100};
-          cway[cur_lru].dtable[rq_index].tag = rq_tag;
-          cway[cur_lru].dtable[rq_index].block[1] = ccif.dload[CPUID];
-          cway[cur_lru].dtable[rq_index].dirty = 0;
 
-          $display("Fetching 2 %h", ccif.daddr);
+         // $display("Fetching 2 %h", ccif.daddr);
 
-          $display("Fetch 2 : lru = %d, idx = %d", cur_lru, rq_index);
+        //  $display("Fetch 2 : lru = %d, idx = %d", cur_lru, rq_index);
 
           if(!ccif.dwait) begin
-            cway[cur_lru].dtable[rq_index].valid = 1;
-            $display("Setting valid  %h",  ccif.daddr);
-
+            write_valid = 1;
+           // $display("Setting valid  %h",  ccif.daddr);
           end
 
           if(hit_out && dpif.dmemWEN) begin
                 $display("WRITING SW lru = %d, idx = %d", cur_lru, rq_index);
-                cway[cur_lru].dtable[rq_index].block[rq_blockoffset] = dpif.dmemstore;
-                cway[cur_lru].dtable[rq_index].dirty = 1;
-                cway[cur_lru].dtable[rq_index].valid = 1;
-                LRU[rq_index] = hit0;
+                
+                CACHE_WEN = 1;
+                write_dirty = 1;
+                write_valid = 1;
+                which_word = rq_blockoffset;
+                write_data = dpif.dmemstore;
+                write_tag = rq_tag;
+
+                // cway[cur_lru].dtable[rq_index].block[rq_blockoffset] = dpif.dmemstore;
+                // cway[cur_lru].dtable[rq_index].dirty = 1;
+                // cway[cur_lru].dtable[rq_index].valid = 1;
           end
+
+          // LRU[rq_index] = !LRU[rq_index];
+
       end
       wb1: begin
           //writeback data in table to RAM
+
+          CACHE_WEN = 0;
+
           ccif.dWEN = 1;
-          ccif.dstore[CPUID] = cway[(hit0 ? 1 : 0)].dtable[rq_index].block[0];
+          ccif.dstore[CPUID] = cway[cur_lru].dtable[rq_index].block[0]; //cur_lru ---> (hit0 ? 1 : 0)
       end
       wb2: begin
+                CACHE_WEN = 0;
+
           ccif.dWEN = 1;
-          ccif.dstore[CPUID] = cway[(hit0 ? 1 : 0)].dtable[rq_index].block[1];
+          ccif.dstore[CPUID] = cway[cur_lru].dtable[rq_index].block[1]; //(hit0 ? 1 : 0)
       end
       default: begin
+             CACHE_WEN = 0;
             //dont do anything
             ccif.dREN = 0;
             ccif.dWEN = 0;
+
+            write_dirty = write_dirty;
+            write_valid = write_valid;
+            which_word = which_word;
+            write_data = write_data;
+            write_tag = write_tag;
       end
     endcase
   end
 
+
+  always_ff @ (posedge CLK, negedge nRST) begin : word
+      if(!nRST) begin
+          cway <= '0;
+      end else if(CACHE_WEN) begin
+          cway[cur_lru].dtable[rq_index].tag <= write_tag;
+          cway[cur_lru].dtable[rq_index].block[which_word] <= write_data; //ccif.dload[CPUID]
+          cway[cur_lru].dtable[rq_index].valid <= write_valid;
+          cway[cur_lru].dtable[rq_index].dirty <= write_dirty;
+      end 
+  end
+
+
+
   always_ff @ (posedge CLK, negedge nRST) begin : ff_fsm
     if(!nRST) begin
-        state <= reset;
+        state <= idle;
     end else begin
         state <= next_state;
-   end
+    end
   end
 
 endmodule
